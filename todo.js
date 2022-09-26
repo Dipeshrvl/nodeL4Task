7aa65775e8e00b5e47fa039f515e675e5b6f5cf4
Dipeshrvl <dipeshraval0007@gmail.com>
2022-09-25 23:24:47 -0700

    msg

--- todo.js
+++ todo.js
@@ -1,45 +1,45 @@
 const todoList = () => {
-    let all = [];
-    let today = new Date().toISOString().split("T")[0];
-    const add = (todoItem) => {
-      all.push(todoItem);
-    };
-    const markAsComplete = (index) => {
-      all[index].completed = true;
-    };
-  
-    const overdue = () => {
-      return all.filter((todo) => todo.dueDate < today);
-    };
-  
-    const dueToday = () => {
-      return all.filter((todo) => todo.dueDate == today);
-    };
-  
-    const dueLater = () => {
-      return all.filter((todo) => todo.dueDate > today);
-    };
-  
-    const toDisplayableList = (list) => {
-      return list
-        .map((todo) => {
-          return `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
-            todo.dueDate === today ? "" : todo.dueDate
-          }`.trim();
-        })
-        .join("\n");
-    };
-  
-    return {
-      all,
-      add,
-      markAsComplete,
-      overdue,
-      dueToday,
-      dueLater,
-      toDisplayableList,
-    };
+  let all = [];
+  let today = new Date().toISOString().split("T")[0];
+  const add = (todoItem) => {
+    all.push(todoItem);
   };
-  
-  module.exports = todoList;
-  
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todo) => todo.dueDate < today);
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => todo.dueDate == today);
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => todo.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map((todo) => {
+        return `${todo.completed ? "[x]" : "[ ]"} ${todo.title} ${
+          todo.dueDate === today ? "" : todo.dueDate
+        }`.trim();
+      })
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+    toDisplayableList,
+  };
+};
+let hello = "hello";
+hello.toLowerCase();
+module.exports = todoList;
